3dd3d29d4713c9bdb27301f3716e18144b8281e1
rdfn37 <88458654+rdfn37@users.noreply.github.com>
2022-01-17 18:01:01 -0300

    First commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'filmflix-522f2',
+    appId: '1:503173646194:web:b6bee1ef35e810ce18ffeb',
+    storageBucket: 'filmflix-522f2.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyB-_Sf4Pl3_6h8heil9hKUOIOGNTythb5g',
+    authDomain: 'filmflix-522f2.firebaseapp.com',
+    messagingSenderId: '503173646194',
+  },
   production: false
 };
 
