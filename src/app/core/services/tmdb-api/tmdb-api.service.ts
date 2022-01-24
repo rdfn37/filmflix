f554019131ac2acc369a1ee831752be67329b4d6
rdfn37 <88458654+rdfn37@users.noreply.github.com>
2022-01-24 18:08:19 -0300

    Implementações na home. Consumindo a TMDB API.

--- src/app/core/services/tmdb-api/tmdb-api.service.ts
+++ src/app/core/services/tmdb-api/tmdb-api.service.ts
@@ -1,15 +1,45 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { MovieTvBase } from '../../models/movie-tv-base';
+
+type ApiResponse = {
+  page: number,
+  results: MovieTvBase[]
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class TmdbApiService {
-  baseUrl = 'https://api.themoviedb.org/3/';
+  baseUrl = 'https://api.themoviedb.org/3';
 
   options = {
-    api_key: 'ef1b01344ec9249024a006c8808cc8c1',
+    api_key: '063624b8cd39d382dfb5084bd7a5ed03',
     language: 'pt-BR'
   }
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  trending(): Observable<MovieTvBase[]> {
+    return this.http.get<ApiResponse>(`${this.baseUrl}/trending/all/week`, {
+      params: this.options
+    })
+      .pipe(
+        map(data => data.results)
+      )
+  }
+
+  search(query: string): Observable<MovieTvBase[]> {
+    return this.http.get<ApiResponse>(`${this.baseUrl}/search/multi`, {
+      params: {
+        ...this.options,
+        include_adult: false,
+        query
+      }
+    })
+      .pipe(
+        map(data => data.results)
+      )
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { debounceTime, distinctUntilChanged, filter, fromEvent, Observable, tap } from 'rxjs';
+
+import { MovieTvBase } from './../core/models/movie-tv-base';
+import { TmdbApiService } from './../core/services/tmdb-api/tmdb-api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+  trending$!: Observable<MovieTvBase[]>;
+  results$?: Observable<MovieTvBase[]>;
+  readonly placeHolder = "http://www.mdtop.com.br/wp-content/uploads/2021/01/placeholder-images-image_large.png"
 
-  constructor() { }
+  createImageLink(poster: string) {
+    if(poster) {
+      return `https://image.tmdb.org/t/p/w300/${poster}`
+    }
 
-  ngOnInit(): void {
+    return this.placeHolder
   }
 
+  @ViewChild('searchInput') searchInput!: ElementRef;
+
+  constructor(private tmdbApiService: TmdbApiService) {}
+
+  ngAfterViewInit(): void {
+    fromEvent(this.searchInput.nativeElement, 'keyup').pipe(
+      filter(Boolean),
+      debounceTime(500),
+      distinctUntilChanged(),
+      tap(() => {
+        const query = this.searchInput.nativeElement.value;
+        console.log(query)
+        if(query) {
+          this.results$ = this.tmdbApiService.search(query)
+        }else{
+          this.results$ = undefined;
+        }
+      })
+    ).subscribe();
+  }
+
+  ngOnInit(): void {
+    this.trending$ = this.tmdbApiService.trending();
+  }
 }
